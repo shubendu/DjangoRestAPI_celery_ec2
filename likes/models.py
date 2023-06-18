de578f6c6289e9d148162e40891a56f0e488188d
shubendu <biswasshubendu4@gmail.com>
2023-06-18 16:17:34 +0530

    added all the data models

--- likes/models.py
+++ likes/models.py
@@ -1,16 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-
-class TaggedItem(models.Model):
-    #what tagged applied to what object
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    #generic relationship
-    #Type (product, video, article)
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    #ID
     object_id = models.PositiveBigIntegerField()
     content_object = GenericForeignKey()
