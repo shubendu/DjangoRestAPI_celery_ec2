bdb62c3971a199d5209030a25e0c393473ad17d3
shubendu <biswasshubendu4@gmail.com>
2023-06-28 08:09:48 +0530

    advance orm

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, Customer
 
 
 
 def say_hello(request):
 
 
-    query_set = Product.objects.filter(title__icontains='coffee')
+    #query_set = Product.objects.filter(last_update__year=2021)
 
-    return render(request,"hello.html", {"name":"shubendu", "products": list(query_set)})
+    products = Product.objects.values_list("id","title","collection__title")
+
+    #query_set = Product.objects.filter(title__icontains='coffee')
+
+    return render(request,"hello.html", {"name":"shubendu", "products": list(products)})
